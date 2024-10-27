30a4001e7199275e7d1200b853645596c0cbf825
Ibrahim Kusumah Kusnanto <ibkusnanto@gmail.com>
2024-10-27 13:15:05 +0800

    chore: Added CORS

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import express from "express";
 import QuotesController from "./controllers/QuotesController";
+import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(cors());
 
 app.get("/api/quote", QuotesController.getDailyQuote);
 
